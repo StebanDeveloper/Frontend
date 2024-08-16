a726df272785dd0df9cae25dd2370b35049198ae
Steban Gonzalez <gsteban101@gmail.com>
2024-08-16 12:37:15 -0500

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { ParkingComponent } from './components/parking/parking.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/vehicles', pathMatch: 'full' }, // Ruta por defecto
+    { path: 'vehicles', component: VehiclesComponent }, // Lista de vehículos
+    { path: 'transaction', component: TransactionComponent }, // Entrada de nuevos vehículos
+    { path: 'spots', component: ParkingComponent } // Lista de plazas de estacionamiento
+  ];
+  
